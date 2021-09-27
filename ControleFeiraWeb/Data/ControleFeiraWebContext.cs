7cb56c3ad32b374f46ffe3a783051a5d61491ba3
luispauloads8 <luispaulo.ads8@gmail.com>
2021-09-27 10:23:18 -0300

    crud scaffolding

---
+++ ControleFeiraWeb/Data/ControleFeiraWebContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ControleFeiraWeb.Models;
+
+namespace ControleFeiraWeb.Data
+{
+    public class ControleFeiraWebContext : DbContext
+    {
+        public ControleFeiraWebContext (DbContextOptions<ControleFeiraWebContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ControleFeiraWeb.Models.Funcionario> Funcionario { get; set; }
+    }
+}
