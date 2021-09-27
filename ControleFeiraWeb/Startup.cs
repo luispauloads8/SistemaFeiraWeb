62682b6afca99293b4e6ad9201cc4af5dbef0df2
luispauloads8 <luispaulo.ads8@gmail.com>
2021-09-27 10:56:40 -0300

    MySQL adaptation and first migration

--- ControleFeiraWeb/Startup.cs
+++ ControleFeiraWeb/Startup.cs
@@ -37,7 +37,8 @@ namespace ControleFeiraWeb
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<ControleFeiraWebContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("ControleFeiraWebContext")));
+                    options.UseMySql(Configuration.GetConnectionString("ControleFeiraWebContext"), Builder =>
+                    Builder.MigrationsAssembly("ControleFeiraWeb")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
