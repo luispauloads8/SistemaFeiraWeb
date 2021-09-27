c3865be3cd816474489dbb5ff28d8cbf1b1ec2af
luispauloads8 <luispaulo.ads8@gmail.com>
2021-09-27 10:15:03 -0300

    Dleting view and controller

--- ControleFeiraWeb/Models/Funcionario.cs
+++ ControleFeiraWeb/Models/Funcionario.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,9 +10,13 @@ namespace ControleFeiraWeb.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:###-##-####}")]
         public string CPF { get; set; }
         public string RG { get; set; }
         public string Email { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd MMM yyyy}")]
         public DateTime Data_Nascimento { get; set; }
         public string Telefone_Celular { get; set; }
         public string Endereco { get; set; }
